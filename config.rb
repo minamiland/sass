bc4ca1e851643973aa83080bc174b1b1a5780048
MMINI <minamiland@gmail.com>
2016-06-22 18:31:47 +0900

    first

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "common/css"
+sass_dir = "common/sass"
+images_dir = "images"
+javascripts_dir = "common/js"
+fonts_dir = "common/fonts"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
